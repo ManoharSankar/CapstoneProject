857ab7bbfc6ba2aa08de9927d5c7b8898dce2760
ManoharSankar <manoharsanakr93@gmail.com>
2024-10-20 18:41:51 +0530

    modified branch

--- build.sh
+++ build.sh
@@ -4,11 +4,15 @@ IMAGE_NAME="capstoneproject_guvi"
 IMAGE_TAG="latest"
 DEV_REPO="manoharms/reactapp-dev"
 PROD_REPO="manoharms/reactapp-prod"
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-echo "CurrentBranch=$BRANCH"
+#BRANCH=$(git rev-parse --abbrev-ref HEAD)
+#echo "CurrentBranch=$BRANCH"
+BRANCH=$(git symbolic-ref --short HEAD 2>/dev/null || git describe --tags --exact-match 2>/dev/null || echo "DETACHED")
+echo "Current branch: $BRANCH"
+
 COMPOSE_FILE="docker-compose.yml"
 
 # Build Docker image
+
 docker build -t $IMAGE_NAME:$IMAGE_TAG .
 
 # Push to Docker Hub based on the current branch
