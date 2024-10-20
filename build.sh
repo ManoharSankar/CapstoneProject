d2fd682345372be5ff454e8837497a05ed5d0dcf
ManoharSankar <manoharsanakr93@gmail.com>
2024-10-20 19:40:49 +0530

    modified

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ IMAGE_TAG="latest"
 DEV_REPO="manoharms/reactapp-dev"
 PROD_REPO="manoharms/reactapp-prod"
 #BRANCH=$(git rev-parse --abbrev-ref HEAD)
-BRANCH=$BRANCH
+BRANCH=$env.BRANCH
 echo "CurrentBranch=$BRANCH"
 
 
