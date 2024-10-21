e834cc6b25f09993b0c550b693fe633b248cbebc
ManoharSankar <manoharsanakr93@gmail.com>
2024-10-21 17:50:34 +0530

    modified jenkins

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,7 @@ COMPOSE_FILE="docker-compose.yml"
 BRANCH_NAME="$1"
 echo "$BRANCH_NAME"
 BRANCH_NAME="${BRANCH_NAME#origin/}"
-# Stop and remove existing container
-docker stop $CONTAINER_NAME || true
-docker rm $CONTAINER_NAME || true
+
 
 #Running the containers
 #docker-compose -f $COMPOSE_FILE up
@@ -24,6 +22,9 @@ if [[ "$BRANCH_NAME" == "dev" && "$DEV_REPO" == "manoharms/guviapp-dev" ]]; then
 elif [[ "$BRANCH_NAME" == "main" && "$PROD_REPO" == "manoharms/guviapp-prod" ]]; then
     docker pull $PROD_REPO:$IMAGE_TAG
     #docker run -d -p 80:80 --name $CONTAINER_NAME $PROD_REPO:$IMAGE_TAG
+    # Stop and remove existing container
+    docker stop $CONTAINER_NAME || true
+    docker rm $CONTAINER_NAME || true
     docker-compose -f $COMPOSE_FILE up -d
 else
     echo "No valid branch for deployment."
