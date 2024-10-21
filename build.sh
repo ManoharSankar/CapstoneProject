ae8e26cec129a931ad5a2bb5c4e7427af487ce47
ManoharSankar <manoharsanakr93@gmail.com>
2024-10-21 18:30:26 +0530

    changes in files

--- build.sh
+++ build.sh
@@ -7,19 +7,19 @@ PROD_REPO="manoharms/guviapp-prod"
 BRANCH_NAME="$1"
 echo "$BRANCH_NAME"
 BRANCH_NAME="${BRANCH_NAME#origin/}"
-COMPOSE_FILE="docker-compose.yml"
-
+DEV_COMPOSE_FILE="docker-compose-dev.yml"
+PROD_COMPOSE_FILE="docker-compose-prod.yml"
 # Build Docker image
 
 #docker build -t $IMAGE_NAME:$IMAGE_TAG .
 
 # Push to Docker Hub based on the current branch
 if [[ "$BRANCH_NAME" == "dev" && "$DEV_REPO" == "manoharms/guviapp-dev" ]]; then
-    docker-compose -f $COMPOSE_FILE --build
+    docker-compose -f $DEV_COMPOSE_FILE --build
     docker tag $IMAGE_NAME:$IMAGE_TAG $DEV_REPO:$IMAGE_TAG
     docker push $DEV_REPO:$IMAGE_TAG
 elif [[ "$BRANCH_NAME" == "main" && "$PROD_REPO" == "manoharms/guviapp-prod" ]]; then
-    docker-compose -f $COMPOSE_FILE --build
+    docker-compose -f $PROD_COMPOSE_FILE --build
     docker tag $IMAGE_NAME:$IMAGE_TAG $PROD_REPO:$IMAGE_TAG
     docker push $PROD_REPO:$IMAGE_TAG
 else
--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,30 @@ set -x
 IMAGE_TAG="latest"
 DEV_REPO="manoharms/guviapp-dev"
 PROD_REPO="manoharms/guviapp-prod"
-CONTAINER_NAME="guvi-app-container"
-COMPOSE_FILE="docker-compose.yml"
+DEV_CONTAINER_NAME="guvi-app-container-dev"
+PROD_CONTAINER_NAME="guvi-app-container-prod"
+DEV_COMPOSE_FILE="docker-compose-dev.yml"
+PROD_COMPOSE_FILE="docker-compose-prod.yml"
 BRANCH_NAME="$1"
 echo "$BRANCH_NAME"
 BRANCH_NAME="${BRANCH_NAME#origin/}"
-# Stop and remove existing container
-docker stop $CONTAINER_NAME || true
-docker rm $CONTAINER_NAME || true
 
 # Pull and run the correct Docker image
 if [[ "$BRANCH_NAME" == "dev" && "$DEV_REPO" == "manoharms/guviapp-dev" ]]; then
     docker pull $DEV_REPO:$IMAGE_TAG
     #docker run -d -p 80:80 --name $CONTAINER_NAME $DEV_REPO:$IMAGE_TAG
-    docker-compose -f $COMPOSE_FILE up -d
+    # Stop and remove existing container
+    docker stop $DEV_CONTAINER_NAME || true
+    docker rm $DEV_CONTAINER_NAME || true
+    docker-compose -f $DEV_COMPOSE_FILE up -d
 
-#elif [[ "$BRANCH_NAME" == "main" && "$PROD_REPO" == "manoharms/guviapp-prod" ]]; then
- #   docker pull $PROD_REPO:$IMAGE_TAG
+elif [[ "$BRANCH_NAME" == "main" && "$PROD_REPO" == "manoharms/guviapp-prod" ]]; then
+    docker pull $PROD_REPO:$IMAGE_TAG
     #docker run -d -p 80:80 --name $CONTAINER_NAME $PROD_REPO:$IMAGE_TAG
-    #docker-compose -f $COMPOSE_FILE up -d
+    # Stop and remove existing container
+    docker stop $PROD_CONTAINER_NAME || true
+    docker rm $PROD_CONTAINER_NAME || true
+    docker-compose -f $PROD_COMPOSE_FILE up -d
 else
     echo "No valid branch for deployment."
 fi
